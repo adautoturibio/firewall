a8c655ad63e2c67691fbe940ef10f4470dea629a
Adauto Turíbio <62852333+adautoturibio@users.noreply.github.com>
2024-04-01 16:48:36 -0300

    questao 4

--- firewall.sh
+++ firewall.sh
@@ -10,9 +10,13 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 
-#III.
+# III.
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth2 -s 10.1.1.0/24 -j MASQUERADE
 
 iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 443 -j ACCEPT
+
+# IV.
+iptables -A FORWARD -m string --string "games" --algo kmp -j LOG --log-prefix "Blocked: "
+iptables -A FORWARD -m string --string "games" --algo kmp -j DROP
