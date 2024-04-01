03f132660b60fb1dfbd0bf25d190266fd0f35e9f
Adauto Turíbio <62852333+adautoturibio@users.noreply.github.com>
2024-04-01 16:41:30 -0300

    questão 3

--- firewall.sh
+++ firewall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 iptables -F
 
 # I.
@@ -7,3 +9,10 @@ iptables -A OUTPUT -o lo -j ACCEPT
 # II.
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
+
+#III.
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth2 -s 10.1.1.0/24 -j MASQUERADE
+
+iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport 443 -j ACCEPT
